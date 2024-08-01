af13de1d49b8511809560caf11417a610cfca192
Itba Ridwanul Haq <122500185+opos123@users.noreply.github.com>
2024-08-01 03:17:39 -0700

    Update mainwindow.cpp

--- ac/mainwindow.cpp
+++ ac/mainwindow.cpp
@@ -5,7 +5,7 @@ MainWindow::MainWindow(QObject *parent) : QObject(parent),
     m_yaw(0.0), m_pitch(0.0), m_roll(0.0), m_latitude(0.0), m_longitude(0.0)
 {
     // Configure the serial port
-    m_serial.setPortName("COM7"); // Sesuaikan nama port sesuai kebutuhan
+    m_serial.setPortName("COM5"); // Sesuaikan nama port sesuai kebutuhan
     m_serial.setBaudRate(QSerialPort::Baud9600);
     m_serial.setDataBits(QSerialPort::Data8);
     m_serial.setParity(QSerialPort::NoParity);
@@ -31,11 +31,13 @@ void MainWindow::readData()
             m_yaw = obj["yaw"].toDouble();
             m_pitch = obj["pitch"].toDouble(); // Tambahkan membaca nilai pitch
             m_roll = obj["roll"].toDouble();   // Tambahkan membaca nilai roll
+            m_accuracy = obj["accuracy"].toDouble();
             m_latitude = obj["latitude"].toDouble();
             m_longitude = obj["longitude"].toDouble();
             emit yawChanged();
             emit pitchChanged();   // Emit signal ketika nilai pitch berubah
             emit rollChanged();    // Emit signal ketika nilai roll berubah
+            emit accuracyChanged();
             emit latitudeChanged();
             emit longitudeChanged();
         }
